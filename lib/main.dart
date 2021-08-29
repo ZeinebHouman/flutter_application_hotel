41fc9318a3ba4b82a04134d98c9f3d62351b1508
ZeinebHouman <76817733+ZeinebHouman@users.noreply.github.com>
2021-08-30 01:32:09 +0200

    Creation of AppBar Widget
    Creation of AppBar Widget

--- lib/main.dart
+++ lib/main.dart
@@ -37,16 +37,29 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget{
   {
     return AppBar(
       leading: IconButton(
-      icon: Icon(Icons.arrow_back,
+      icon: Icon(Icons.arrow_back, 
                   color: Colors.grey[800],
                   size: 20 ),
       onPressed: null), 
-
-      title: Text("Explore", style: GoogleFonts.nunito(
+      centerTitle: true,
+      title: Text("Explore", 
+      style: GoogleFonts.nunito( 
         color: Colors.black,
         fontSize:  22,
         fontWeight: FontWeight.w800
       )),
+      actions: [
+      IconButton(
+      icon: Icon(Icons.favorite_outline_rounded, 
+                  color: Colors.grey[800],
+                  size: 20 ),
+      onPressed: null),
+      IconButton(
+      icon: Icon(Icons.place, 
+                  color: Colors.grey[800],
+                  size: 20 ),
+      onPressed: null)
+      ],
       backgroundColor: Colors.white,
     );
   } 
