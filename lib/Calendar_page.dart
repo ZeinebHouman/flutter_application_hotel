c6c309d62fb885ea6b1e758be50cc6c9911b3abe
ZeinebHouman <76817733+ZeinebHouman@users.noreply.github.com>
2021-09-02 00:27:34 +0200

    Update Calendar_page.dart

--- lib/Calendar_page.dart
+++ lib/Calendar_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:google_fonts/google_fonts.dart';
+DateTime kNow = DateTime.now();
+DateTime kFirstDay = DateTime(kNow.year, kNow.month - 3, kNow.day);
+DateTime kLastDay = DateTime(kNow.year, kNow.month + 3, kNow.day);
+
+const d_green = const Color(0xFF54D3C2);
 class CalendarPage extends StatelessWidget{
   @override
   Widget build(BuildContext context)
@@ -8,7 +13,7 @@ class CalendarPage extends StatelessWidget{
     return Scaffold(
       appBar: MyAppBar(),
       body: SingleChildScrollView(
-        child: Column(children: [],) 
+        child: Column(children: [ PeriodSection(),CalendarRange()],) 
       ,)
 
     );
@@ -35,4 +40,72 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget{
     );
   } 
 
+}
+class PeriodSection extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Column(children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [ 
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+            Text('Depart', textAlign: TextAlign.left, style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400, color: Colors.grey[700]),
+            ),
+            SizedBox(height:4),
+            Text('Month 12 Dec', textAlign: TextAlign.left, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+
+          ],),
+          //Pour le trait vertical
+          Container( height: 60 , width: 1, color : Colors.grey[350],),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+               Text('Return', textAlign: TextAlign.left, style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400, color: Colors.grey[700]),
+            ),
+            SizedBox(height:4),
+            Text('Tues 22 Dec', textAlign: TextAlign.left, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),)
+
+          
+
+
+          ],),
+           //Pour le trait horizontale
+           Divider(
+             color: Colors.blue,
+             height: 1,
+           )
+
+
+        ],
+
+
+      )
+    ]);
+  }
+}
+//Statful car  le contenu de note section sera modifié en fonction des actions de l’utilisateur, notamment lors de la sélection des jours sur le calendrier
+class CalendarRange extends StatefulWidget{
+  @override
+   _CalendarRangeState createState() => _CalendarRangeState();
+}
+class _CalendarRangeState extends State<CalendarRange>{
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  RangeSelectionMode _rangeSelectionMode=RangeSelectionMode.toggledOn;
+  DateTime _focusedDay=DateTime.now();
+  DateTime? _selectdDay;
+  DateTime? _randeStart;
+   DateTime? _randeEnd;
+
+
+   @override
+   Widget build(BuildContext context)
+   {
+     return Container()
+   }
 }
